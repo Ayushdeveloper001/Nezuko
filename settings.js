c837a8a385461968a9fa2f0c3b43235c18ceec30
Ayush <117559953+Ayushdeveloper001@users.noreply.github.com>
2023-05-11 14:32:24 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,11 +3,11 @@ const chalk = require("chalk");
 require("dotenv").config();
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "917003213983, 923087880256";
+  gg = "16468338933";
 }
 
 global.owner = gg.split(",");
-global.mongodb = process.env.MONGODB || "none";
+global.mongodb = process.env.MONGODB || "mongodb://mongo:nTdbpvmcgtJtMkpw2c2r@containers-us-west-117.railway.app:5965";
 global.prefa = process.env.PREFIX || "."
 global.ggle = process.env.GOOGLE_API || "none"
 global.wter = process.env.WEATHER_API || "none"
